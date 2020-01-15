f49c504099c4250b8382ef59cce71955088e7aed
Debasis Mohapatra <ds.tuation@gmail.com>
2020-01-15 10:37:48 +0530

    added main and test class

---
+++ src/test/java/com/tuation/student/service/StudentServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tuation.student.service;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
