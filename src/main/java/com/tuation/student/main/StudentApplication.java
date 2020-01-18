ba8cbe1b424b94aa6e89eef203952319bb1aad58
Debasis Mohapatra <ds.tuation@gmail.com>
2020-01-18 13:12:26 +0530

    Change package name and class name

--- src/main/java/com/tuation/student/main/StudentApplication.java
+++ src/main/java/com/tuation/student/main/StudentApplication.java
@@ -1,12 +1,12 @@
-package com.tuation.student.service;
+package com.tuation.student.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentServiceApplication {
+public class StudentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentServiceApplication.class, args);
+		SpringApplication.run(StudentApplication.class, args);
 	}
 
 }
